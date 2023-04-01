243a090c3e66cb449aa21076a3b4b88c01361143
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-04-01 13:12:48 +0700

    Add files via upload

---
+++ Latihan3/luas dan keliling lingkaran/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim jari_jari, luas, keliling As Integer
+
+        jari_jari = Val(txtJari_jari.Text)
+
+        luas = 3.14 * jari_jari * jari_jari
+        keliling = 2 * 3.14 * jari_jari
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+End Class
