db58db283b99950d83e745c0183c6600fbbf29d0
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-04-01 13:10:25 +0700

    Add files via upload

---
+++ Latihan3/luas dan keliling Segitiga/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAlas = New System.Windows.Forms.TextBox()
+        Me.txtTinggi = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSisia = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSisib = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSisic = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.txtLuas = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtKeliling = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Alas"
+        '
+        'txtAlas
+        '
+        Me.txtAlas.Location = New System.Drawing.Point(95, 29)
+        Me.txtAlas.Name = "txtAlas"
+        Me.txtAlas.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlas.TabIndex = 1
+        '
+        'txtTinggi
+        '
+        Me.txtTinggi.Location = New System.Drawing.Point(95, 57)
+        Me.txtTinggi.Name = "txtTinggi"
+        Me.txtTinggi.Size = New System.Drawing.Size(100, 22)
+        Me.txtTinggi.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Tinggi"
+        '
+        'txtSisia
+        '
+        Me.txtSisia.Location = New System.Drawing.Point(95, 94)
+        Me.txtSisia.Name = "txtSisia"
+        Me.txtSisia.Size = New System.Drawing.Size(100, 22)
+        Me.txtSisia.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Sisi a"
+        '
+        'txtSisib
+        '
+        Me.txtSisib.Location = New System.Drawing.Point(95, 132)
+        Me.txtSisib.Name = "txtSisib"
+        Me.txtSisib.Size = New System.Drawing.Size(100, 22)
+        Me.txtSisib.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 132)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Sisi b"
+        '
+        'txtSisic
+        '
+        Me.txtSisic.Location = New System.Drawing.Point(95, 170)
+        Me.txtSisic.Name = "txtSisic"
+        Me.txtSisic.Size = New System.Drawing.Size(100, 22)
+        Me.txtSisic.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 170)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 17)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Sisi c"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnHitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHitung.Location = New System.Drawing.Point(95, 209)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(100, 34)
+        Me.btnHitung.TabIndex = 10
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = False
+        '
+        'txtLuas
+        '
+        Me.txtLuas.Location = New System.Drawing.Point(95, 259)
+        Me.txtLuas.Name = "txtLuas"
+        Me.txtLuas.Size = New System.Drawing.Size(100, 22)
+        Me.txtLuas.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 259)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 17)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Luas"
+        '
+        'txtKeliling
+        '
+        Me.txtKeliling.Location = New System.Drawing.Point(95, 298)
+        Me.txtKeliling.Name = "txtKeliling"
+        Me.txtKeliling.Size = New System.Drawing.Size(100, 22)
+        Me.txtKeliling.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 298)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(61, 17)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Keliling"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(434, 415)
+        Me.Controls.Add(Me.txtKeliling)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtLuas)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtSisic)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSisib)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtSisia)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtTinggi)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtAlas)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtAlas As TextBox
+    Friend WithEvents txtTinggi As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSisia As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtSisib As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSisic As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtKeliling As TextBox
+    Friend WithEvents Label7 As Label
+End Class
---
+++ Latihan3/luas dan keliling Segitiga/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim alas, tinggi, sisi_a, sisi_b, sisi_c, luas, keliling As Integer
+
+        alas = Val(txtAlas.Text)
+        tinggi = Val(txtTinggi.Text)
+        sisi_a = Val(txtSisia.Text)
+        sisi_b = Val(txtSisib.Text)
+        sisi_c = Val(txtSisic.Text)
+
+        luas = 1 / 2 * alas * tinggi
+        keliling = sisi_a + sisi_b + sisi_c
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+
+    End Sub
+End Class
