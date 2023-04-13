04f379fa1f7663ab6bd3814583ec8a5c3d9ca92b
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-04-13 22:19:34 +0700

    Add files via upload

---
+++ Latihan 5/Latihan 5c/Latihan 5c/Latihan 5c/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtKodeJabatan = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(141, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 17)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Ibnu Hasan"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(144, 57)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(83, 29)
+        Me.btnSubmit.TabIndex = 15
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Location = New System.Drawing.Point(21, 129)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(308, 22)
+        Me.txtKeterangan.TabIndex = 14
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 17)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Keterangan"
+        '
+        'txtKodeJabatan
+        '
+        Me.txtKodeJabatan.Location = New System.Drawing.Point(21, 60)
+        Me.txtKodeJabatan.Name = "txtKodeJabatan"
+        Me.txtKodeJabatan.Size = New System.Drawing.Size(110, 22)
+        Me.txtKodeJabatan.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 17)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Kode Jabatan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Info
+        Me.ClientSize = New System.Drawing.Size(422, 241)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtKodeJabatan)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtKodeJabatan As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Latihan 5/Latihan 5c/Latihan 5c/Latihan 5c/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtKodeJabatan.Text
+            Case "MGR" : txtKeterangan.Text = "Manager"
+
+            Case "SKR" : txtKeterangan.Text = "Sekretaris"
+
+            Case "BDH" : txtKeterangan.Text = "Bendahara"
+
+            Case Else : txtKeterangan.Text = "Staff"
+        End Select
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
