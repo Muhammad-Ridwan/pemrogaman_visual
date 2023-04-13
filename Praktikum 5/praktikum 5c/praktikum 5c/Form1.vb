2e3b266e7ccd5db67a061e91061362aff41df449
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-04-13 22:25:40 +0700

    Add files via upload

---
+++ Praktikum 5/praktikum 5c/praktikum 5c/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtKodeMakanan.Text
+            Case "NG" : txtKeterangan.Text = "Nasi Goreng"
+
+            Case "MG" : txtKeterangan.Text = "Mie Goreng"
+
+            Case "NP" : txtKeterangan.Text = "Nasi Padang"
+
+            Case Else : txtKeterangan.Text = "Tidak Dikenali"
+        End Select
+    End Sub
+End Class
