17c0419b36620a7dd6c2b15d16b46c70c2529ebb
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-04-30 10:53:54 +0700

    Add files via upload

---
+++ Tugas6/FrmRegistrasi/FrmRegistrasi/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CmbProdi = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtRegistrasi = New System.Windows.Forms.TextBox()
+        Me.txtSpp = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtJas = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.label = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(171, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 17)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(126, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "BIAYA REGISTRASI"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(23, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Prodi"
+        '
+        'CmbProdi
+        '
+        Me.CmbProdi.FormattingEnabled = True
+        Me.CmbProdi.Items.AddRange(New Object() {"Teknik Informatika (S1)", "Teknik Industri (S1)", "Teknik Peternakan (S1)"})
+        Me.CmbProdi.Location = New System.Drawing.Point(129, 97)
+        Me.CmbProdi.Name = "CmbProdi"
+        Me.CmbProdi.Size = New System.Drawing.Size(218, 24)
+        Me.CmbProdi.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(23, 140)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 17)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Registrasi"
+        '
+        'txtRegistrasi
+        '
+        Me.txtRegistrasi.Location = New System.Drawing.Point(129, 140)
+        Me.txtRegistrasi.Name = "txtRegistrasi"
+        Me.txtRegistrasi.Size = New System.Drawing.Size(163, 22)
+        Me.txtRegistrasi.TabIndex = 5
+        '
+        'txtSpp
+        '
+        Me.txtSpp.Location = New System.Drawing.Point(129, 179)
+        Me.txtSpp.Name = "txtSpp"
+        Me.txtSpp.Size = New System.Drawing.Size(163, 22)
+        Me.txtSpp.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(23, 179)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 17)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "SPP / Smt"
+        '
+        'txtJas
+        '
+        Me.txtJas.Location = New System.Drawing.Point(129, 223)
+        Me.txtJas.Name = "txtJas"
+        Me.txtJas.Size = New System.Drawing.Size(163, 22)
+        Me.txtJas.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(23, 223)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(78, 17)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Jas Almet"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(129, 300)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(163, 22)
+        Me.txtTotal.TabIndex = 11
+        '
+        'label
+        '
+        Me.label.AutoSize = True
+        Me.label.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label.Location = New System.Drawing.Point(23, 300)
+        Me.label.Name = "label"
+        Me.label.Size = New System.Drawing.Size(45, 17)
+        Me.label.TabIndex = 10
+        Me.label.Text = "Total"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHitung.Location = New System.Drawing.Point(129, 251)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(86, 27)
+        Me.btnHitung.TabIndex = 12
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Salmon
+        Me.ClientSize = New System.Drawing.Size(474, 427)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.txtJas)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtSpp)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtRegistrasi)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.CmbProdi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Registras"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CmbProdi As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtRegistrasi As TextBox
+    Friend WithEvents txtSpp As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtJas As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents label As Label
+    Friend WithEvents btnHitung As Button
+End Class
---
+++ Tugas6/FrmRegistrasi/FrmRegistrasi/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CmbProdi.Items.Clear()
+        CmbProdi.Items.Add("Teknik Informatika (S1)")
+        CmbProdi.Items.Add("Teknik Industri (S1)")
+        CmbProdi.Items.Add("Teknik Peternakan (S1)")
+    End Sub
+
+    Private Sub CmbProdi_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbProdi.SelectedIndexChanged
+        If CmbProdi.Text = "Teknik Informatika (S1)" Then
+            txtRegistrasi.Text = "2000000"
+            txtSpp.Text = "2400000"
+            txtJas.Text = "150000"
+            txtTotal.Clear()
+        ElseIf CmbProdi.Text = "Teknik Industri (S1)" Then
+            txtRegistrasi.Text = "2000000"
+            txtSpp.Text = "2700000"
+            txtJas.Text = "150000"
+            txtTotal.Clear()
+        ElseIf CmbProdi.Text = "Teknik Peternakan (S1)" Then
+            txtRegistrasi.Text = "2000000"
+            txtSpp.Text = "2000000"
+            txtJas.Text = "150000"
+            txtTotal.Clear()
+        End If
+    End Sub
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        txtTotal.Text = Val(txtRegistrasi.Text) + Val(txtSpp.Text) + Val(txtJas.Text)
+    End Sub
+End Class
