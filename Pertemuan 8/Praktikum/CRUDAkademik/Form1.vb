acbc9f27025e525482a2db38bfaf7c77386b0ac5
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-07-20 21:41:02 +0700

    Add files via upload

---
+++ Pertemuan 8/Praktikum/CRUDAkademik/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Reload()
+    End Sub
+
+    Private Sub Reload()
+        oMahasiswa.getAllData(DataGridView1)
+    End Sub
+End Class
