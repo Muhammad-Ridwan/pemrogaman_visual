d19c93f5722a05e7f205eb2b52740b5a1671b0ca
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-07-31 10:29:28 +0700

    users_api.php

--- Tugas Besar_Muhammad Ridwan_200511135_D/SQL dan File API/apprawatjalan_api/users_api.php
+++ Tugas Besar_Muhammad Ridwan_200511135_D/SQL dan File API/apprawatjalan_api/users_api.php
@@ -1,127 +1,27 @@
 <?php
 require_once 'database.php';
 require_once 'Users.php';
-
 $db = new MySQLDatabase();
 $users = new Users($db);
-
-$id = 0;
-$username = "";
-
+$id=0;
+$username=0;
 // Check the HTTP request method
 $method = $_SERVER['REQUEST_METHOD'];
-
 // Handle the different HTTP methods
 switch ($method) {
-    case 'GET':
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-        }
-        if (isset($_GET['username'])) {
-            $username = $_GET['username'];
-        }
-        
-        if ($id > 0) {
-            $result = $users->get_by_id($id);
-        } elseif (!empty($username)) {
-            $result = $users->get_by_username($username);
-        } else {
-            $result = $users->get_all();
-        }
-       
-        $val = array();
-        while ($row = $result->fetch_assoc()) {
-            $val[] = $row;
-        }
-        
-        header('Content-Type: application/json');
-        echo json_encode($val);
-        break;
-    
     case 'POST':
-        // Add a new user
+        // Add a new users
         $users->username = $_POST['username'];
         $users->passwd = $_POST['passwd'];
-        $users->rolename = $_POST['rolename'];
-
-        $users->insert();
-        $a = $users->db->affected_rows();
+               
+        $v = $users->login($users->username,$users->passwd);      
         
-        if ($a > 0) {
-            $data['status'] = 'success';
-            $data['message'] = 'Data User created successfully.';
+        if($v!=null){
+            $data['status']='success';
+            $data['message']=$v;
         } else {
-            $data['status'] = 'failed';
-            $data['message'] = 'Data User not created.';
-        }
-        
-        header('Content-Type: application/json');
-        echo json_encode($data);
-        break;
-    
-    case 'PUT':
-        // Update an existing data
-        $_PUT = [];
-        parse_str(file_get_contents("php://input"), $_PUT);
-        
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-        }
-        if (isset($_GET['username'])) {
-            $username = $_GET['username'];
-        }
-        
-        $users->username = $_PUT['username'];
-        $users->passwd = $_PUT['passwd'];
-        $users->rolename = $_PUT['rolename'];
-        
-        if ($id > 0) {
-            $users->update($id);
-        } elseif (!empty($username)) {
-            $users->update_by_username($username);
-        } else {
-            // Handle invalid request
-        } 
-        
-        $a = $users->db->affected_rows();
-        
-        if ($a > 0) {
-            $data['status'] = 'success';
-            $data['message'] = 'Data User updated successfully.';
-        } else {
-            $data['status'] = 'failed';
-            $data['message'] = 'Data User update failed.';
-        }
-        
-        header('Content-Type: application/json');
-        echo json_encode($data);
-        break;
-    
-    case 'DELETE':
-        // Delete a user
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-        }
-        if (isset($_GET['username'])) {
-            $username = $_GET['username'];
-        }
-        
-        if ($id > 0) {
-            $users->delete($id);
-        } elseif (!empty($username)) {
-            $users->delete_by_username($username);
-        } else {
-            // Handle invalid request
-        } 
-        
-        $a = $users->db->affected_rows();
-        
-        if ($a > 0) {
-            $data['status'] = 'success';
-            $data['message'] = 'Data User deleted successfully.';
-        } else {
-            $data['status'] = 'failed';
-            $data['message'] = 'Data User delete failed.';
+            $data['status']='failed';
+            $data['message']='Login is not valid';
         }
         
         header('Content-Type: application/json');
@@ -131,7 +31,6 @@ switch ($method) {
     default:
         header("HTTP/1.0 405 Method Not Allowed");
         break;
-}
-
-$db->close();
+    }
+$db->close()
 ?>
