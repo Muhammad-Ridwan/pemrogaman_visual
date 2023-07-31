78b2cc0e50c18971ee4dd6baac433d1db9424120
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-07-31 10:28:16 +0700

    Users.php

--- Tugas Besar_Muhammad Ridwan_200511135_D/SQL dan File API/apprawatjalan_api/Users.php
+++ Tugas Besar_Muhammad Ridwan_200511135_D/SQL dan File API/apprawatjalan_api/Users.php
@@ -1,6 +1,6 @@
 <?php
+//Simpanlah dengan nama file : Users.php
 require_once 'database.php';
-
 class Users 
 {
     private $db;
@@ -8,67 +8,23 @@ class Users
     public $username = "";
     public $passwd = "";
     public $rolename = "";
-
     public function __construct(MySQLDatabase $db)
     {
         $this->db = $db;
     }
-
-    public function get_all() 
-    {
-        $query = "SELECT * FROM $this->table";
-        $result_set = $this->db->query($query);
-        return $result_set;
+    
+    public function login($uname,$pwd){
+        $passwd = MD5($pwd);
+        $query = "SELECT * FROM $this->table WHERE username = '{$uname}' and passwd='{$passwd}'";
+        $hasil = $this->db->query($query);   
+        $data =  $this->db->fetch($hasil);
+                
+        if($this->db->affected_rows()>0){
+            return $data['rolename'];
+        } else {
+            return null;
+        } 
     }
-
-    public function get_by_id(int $id)
-    {
-        $query = "SELECT * FROM $this->table WHERE id = $id";
-        $result_set = $this->db->query($query);   
-        return $result_set;
-    }
-
-    public function get_by_username(string $username)
-    {
-        $query = "SELECT * FROM $this->table WHERE username = '$username'";
-        $result_set = $this->db->query($query);   
-        return $result_set;
-    }
-
-    public function insert(): int
-    {
-        $query = "INSERT INTO $this->table (`username`, `passwd`, `rolename`) 
-        VALUES ('$this->username', '$this->passwd', '$this->rolename')";
-        $this->db->query($query);
-        return $this->db->insert_id();
-    }
-
-    public function update(int $id): int
-    {
-        $query = "UPDATE $this->table SET username = '$this->username', passwd = '$this->passwd', rolename = '$this->rolename' WHERE id = $id";
-        $this->db->query($query);
-        return $this->db->affected_rows();
-    }
-
-    public function update_by_username(string $username): int
-    {
-        $query = "UPDATE $this->table SET username = '$this->username', passwd = '$this->passwd', rolename = '$this->rolename' WHERE username = '$username'";
-        $this->db->query($query);
-        return $this->db->affected_rows();
-    }
-
-    public function delete(int $id): int
-    {
-        $query = "DELETE FROM $this->table WHERE id = $id";
-        $this->db->query($query);
-        return $this->db->affected_rows();
-    }
-
-    public function delete_by_username(string $username): int
-    {
-        $query = "DELETE FROM $this->table WHERE username = '$username'";
-        $this->db->query($query);
-        return $this->db->affected_rows();
-    }
+    
 }
 ?>
