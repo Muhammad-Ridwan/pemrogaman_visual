dcd12f67a73b333571ce06fbbe0dde381b12298b
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-07-31 15:13:09 +0700

    Add files via upload

---
+++ Tugas Besar_Muhammad Ridwan_200511135_D/apprawatjalan/apprawatjalan/Rawatjalan.vb
@@ -0,0 +1,11 @@
+﻿Public Class Rawatjalan
+    Public Property no_recmd As String
+    Public Property nrp As String
+    Public Property nama As String
+    Public Property nama_dokter As String
+    Public Property biaya As String
+    Public Property hasil_diagnosa As String
+    Public Property harga_obat As String
+    Public Property total As String
+
+End Class
