3d1c9d593f92448b04bd59f43a82bebba9a44ce8
Muhammad Ridwan <103178239+Muhammad-Ridwan@users.noreply.github.com>
2023-07-31 14:23:56 +0700

    Add files via upload

---
+++ Tugas Besar_Muhammad Ridwan_200511135_D/apprawatjalan/apprawatjalan/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub PasienToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasienToolStripMenuItem.Click
+        Form2.ShowDialog()
+    End Sub
+
+    Private Sub RawatjalanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RawatjalanToolStripMenuItem.Click
+        Form3.ShowDialog()
+    End Sub
+
+    Private Sub DataDokterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DataDokterToolStripMenuItem.Click
+        Form4.ShowDialog()
+    End Sub
+
+
+    Private Sub MatikanSemuaMenu()
+        MenuPasienBar.Visible = False
+        MenuRawatJalanBar.Visible = False
+        MenuDokterBar.Visible = False
+    End Sub
+
+
+    Private Sub ExitBar_Click(sender As Object, e As EventArgs) Handles ExitBar.Click
+        End
+    End Sub
+End Class
